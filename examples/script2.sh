196e177a788b3727a9395de8d703c02245bd9f5a
Daniel Blaschke <danblasch@users.sourceforge.net>
2011-01-21 12:28:53 +0100

    patch to gitsync 1.2.1

--- examples/script2.sh
+++ examples/script2.sh
@@ -5,7 +5,11 @@
 # assuming your are working on something in LaTeX, which you are tracking with
 # git and have added "*.pdf" to your .gitignore-file, you might want to call 
 # the following in order to convert eps-graphics files to pdf:
-epstopdf *.eps
+
+for line in $(ls *.eps)
+do
+epstopdf $line
+done
 
 echo "have a nice day!"
 
